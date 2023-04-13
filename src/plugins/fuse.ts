8bb4d550064c58eae7ac0db362a15e12caeb180e
royestone <rodrigo.mata.ib@gmail.com>
2023-04-13 13:42:58 -0600

    fix: testing lint-staged

--- src/plugins/fuse.ts
+++ src/plugins/fuse.ts
@@ -2,7 +2,7 @@ import Fuse from 'fuse.js'
 
 export const createFuse = (locations: Array<{ ref: object; data: { description: string; x: number; z: number } }>) => {
   return new Fuse(locations, {
-    keys: ['data.description'],
+    keys: ["data.description"],
     includeScore: true,
     threshold: 0.3,
   })
