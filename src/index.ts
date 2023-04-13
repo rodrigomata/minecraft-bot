312b223f39a5d5def403d22d0fc740f04bf0a820
royestone <rodrigo.mata.ib@gmail.com>
2023-04-13 12:30:30 -0600

    feat: scaffolds project

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { Client, IntentsBitField } from 'discord.js'
+
+// Initialize discord bot
+const client = new Client({ intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages] })
+
+// When bot is ready
+client.on('ready', () => {
+  console.log('Bot is ready')
+})
+
+// When bot receives a message
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply("Pong!")
+  }
+})
