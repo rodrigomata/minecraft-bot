3ee0f93b07732036f97c7c4fbe6202ff65ff6978
royestone <rodrigo.mata.ib@gmail.com>
2023-04-13 12:43:01 -0600

    fix: removing husky

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import { Client, IntentsBitField } from "discord.js"
+import { Client, IntentsBitField } from 'discord.js'
 
 // Initialize discord bot
-const client = new Client({ intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages] })
+const client = new Client({
+  intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages],
+})
 
 // When bot is ready
-client.on("ready", () => {
+client.on('ready', () => {
   console.log("Bot is ready")
 })
 
 // When bot receives a message
-client.on("messageCreate", message => {
-  if (message.content === "!ping") {
-    message.reply("Pong!")
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply('Pong!')
   }
 })
